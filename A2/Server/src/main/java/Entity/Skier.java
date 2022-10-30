cbe91de1b5364beb2908189612fee27e2f60f1fa
zijing <liu.zijing@northeastern.edu>
2022-10-30 01:01:43 -0700

    add skier.java

---
+++ A2/Server/src/main/java/Entity/Skier.java
@@ -0,0 +1,40 @@
+package Entity;
+
+public class Skier {
+  private int resortId;
+  private String seasonId;
+  private String dayId;
+  private int skierId;
+
+  public int getResortId() {
+    return resortId;
+  }
+
+  public void setResortId(int resortId) {
+    this.resortId = resortId;
+  }
+
+  public String getSeasonId() {
+    return seasonId;
+  }
+
+  public void setSeasonId(String seasonId) {
+    this.seasonId = seasonId;
+  }
+
+  public String getDayId() {
+    return dayId;
+  }
+
+  public void setDayId(String dayId) {
+    this.dayId = dayId;
+  }
+
+  public int getSkierId() {
+    return skierId;
+  }
+
+  public void setSkierId(int skierId) {
+    this.skierId = skierId;
+  }
+}
